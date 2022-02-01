88ed2115b03577c02e5c193ea922e5196f6e5270
Ali Kooshesh <alikooshesh@outlook.com>
2022-02-01 04:28:41 +0330

    connect to API

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Layout from "./layout";
 import {Route, Routes} from "react-router-dom";
 import UserList from "./pages/userList";
 import EditUser from "./pages/editUser";
+import {useDispatch} from "react-redux";
+import axios from "axios";
+import {ApiLink} from "./Api/ApiLink";
+import {add} from "./redux/reducers/dataReducer/dataReducer";
+
 function App() {
+
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        axios.get(ApiLink)
+            .then(data => data.data.map((user : any) => dispatch(add({
+                id : user.id ,
+                name : user.name,
+                email : user.email ,
+                city : user.address.city,
+                userName : user.username
+            }))))
+            .catch(err => console.log(err))
+    },[])
 
   return (
       <Routes>
--- src/redux/reducers/dataReducer/dataReducer.ts
+++ src/redux/reducers/dataReducer/dataReducer.ts
@@ -1,4 +1,6 @@
 import {createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
+import {ApiLink} from "../../../Api/ApiLink";
 
 export interface IuserData{
     id : number,
@@ -29,22 +31,32 @@ const dataReducer = createSlice({
 
             if (userFinder === -1){
                 state.userList.push(newUser)
+                newUser.id > 10 && axios.post(ApiLink,newUser)
+                    .then(data => console.log(data.data))
+                    .catch(err => console.log(err))
             }
         },
         remove: (state, action) => {
             state.userList = state.userList.filter(user => user.id !== action.payload.id)
+            axios.delete(`${ApiLink}/${action.payload.id}`)
+                .then(data => console.log(data.data))
+                .catch(err => console.log(err))
         },
         editUser: (state, action) => {
             const userFinder: number = state.userList.findIndex(user => user.id == action.payload.id)
             if (userFinder >= 0)  {
                 const editedUser: IuserData = {
-                    id : action.payload.id,
+                    id : +action.payload.id,
                     name : action.payload.name,
                     userName : state.userList[userFinder].userName,
                     email : action.payload.email,
                     city : state.userList[userFinder].city
                 }
                 state.userList.splice(userFinder,1,editedUser)
+
+                axios.put(`${ApiLink}/${action.payload.id}`,editedUser)
+                    .then(data => console.log(data.data))
+                    .catch(err => console.log(err))
             }
 
         }
